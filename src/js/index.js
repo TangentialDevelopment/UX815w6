4a52323970b802588d915fca73d954b3d7e4c214
TangentialDevelopment <joetang148@gmail.com>
2025-02-20 21:05:52 -0500

    added effects

---
+++ src/js/index.js
@@ -0,0 +1,21 @@
+ScrollReveal().reveal('.flower1', {
+    delay: 500
+});
+ScrollReveal().reveal('.flower2', {
+    distance: '100px'
+});
+ScrollReveal().reveal('.flower3', {
+    easing: "cubic-bezier(.98,.23,.12,.62)"
+});
+ScrollReveal().reveal('.flower4', {
+    distance: '100px',
+    origin: "right"
+});
+ScrollReveal().reveal('.ocean', {
+    rotate: {
+        x: 20,
+        z: 20
+    },
+    delay: 300,
+    distance: '100px'
+});
